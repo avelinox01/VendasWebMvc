46616a9709e01ca4c6c566d5adef3682d13c6630
avelinox01 <101229630+avelinox01@users.noreply.github.com>
2022-09-27 15:04:38 -0300

    Primeiros testes de páginas de controlador e Razor

--- VendasWebMvc/Controllers/HomeController.cs
+++ VendasWebMvc/Controllers/HomeController.cs
@@ -17,7 +17,7 @@ namespace VendasWebMvc.Controllers
 
         public IActionResult About()
         {
-            ViewData["Message"] = "Your application description page.";
+            ViewData["Message"] = "Vendas Web App com C# Curso";
 
             return View();
         }
